d5d5737f97aad3b236351f4ceb1c04d0f0aaf695
EvanBradford <evan.bradford1@gmail.com>
2018-11-29 19:15:17 -0500

    Fixed file locations and got rid of strange leftover invisible files

--- js/menu_scene.js
+++ js/menu_scene.js
@@ -12,7 +12,7 @@ function createMenu(engine, canvas, message, database) {                        
   var background = new BABYLON.Layer("bg", "res/menu.png", scene, true);        // background layer
   
   //music
-  var music = new BABYLON.Sound("achaidh_cheide", "resources/music/achaidh_cheide.wav", scene, null, { loop: true, autoplay: true });
+  var music = new BABYLON.Sound("achaidh_cheide", "res/music/achaidh_cheide.wav", scene, null, { loop: true, autoplay: true });
   
   // GUI
   var advTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI"); // AdvancedDynamicTexture for the controls of the gui
