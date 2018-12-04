05ca97035ac332821b1d7ba9b6524f00666e759c
Nathan Estep <ne863015@ohio.edu>
2018-12-03 21:40:34 -0500

    This optimized the sound issue slowing the display of assests

--- js/music.js
+++ js/music.js
@@ -19,142 +19,58 @@ function createMusic(engine, canvas, message, database) {                       
 
   //music
   var music1 = new BABYLON.Sound("achaidh_cheide", "res/music/achaidh_cheide.wav", scene, null, { loop: false, autoplay: true});
-  var music2 = new BABYLON.Sound("cartoon_battle", "res/music/cartoon_battle.wav", scene, null, { loop: false, autoplay: false});
-  var music3 = new BABYLON.Sound("celtic_impulse", "res/music/celtic_impulse.wav", scene, null, { loop: false, autoplay: false});
-  var music4 = new BABYLON.Sound("clenched_teeth", "res/music/clenched_teeth.wav", scene, null, { loop: false, autoplay: false});
-  var music5 = new BABYLON.Sound("failing_defense", "res/music/failing_defense.wav", scene, null, { loop: false, autoplay: false});
-  var music6 = new BABYLON.Sound("fiddles_mcGinty", "res/music/fiddles_mcGinty.wav", scene, null, { loop: false, autoplay: false});
-  var music7 = new BABYLON.Sound("galway", "res/music/galway.wav", scene, null, { loop: false, autoplay: false});
-  var music8 = new BABYLON.Sound("hidden_past", "res/music/hidden_past.wav", scene, null, { loop: false, autoplay: false});
-  var music9 = new BABYLON.Sound("one-eyed_maestro", "res/music/one-eyed_maestro.wav", scene, null, { loop: false, autoplay: false});
-  var music10 = new BABYLON.Sound("parisian", "res/music/parisian.wav", scene, null, { loop: false, autoplay: false});
+   var music2 = new BABYLON.Sound("cartoon_battle", "res/music/cartoon_battle.wav", scene, null, { loop: false, autoplay: false});
+  // var music3 = new BABYLON.Sound("celtic_impulse", "res/music/celtic_impulse.wav", scene, null, { loop: false, autoplay: false});
+  // var music4 = new BABYLON.Sound("clenched_teeth", "res/music/clenched_teeth.wav", scene, null, { loop: false, autoplay: false});
+  // var music5 = new BABYLON.Sound("failing_defense", "res/music/failing_defense.wav", scene, null, { loop: false, autoplay: false});
+  // var music6 = new BABYLON.Sound("fiddles_mcGinty", "res/music/fiddles_mcGinty.wav", scene, null, { loop: false, autoplay: false});
+  // var music7 = new BABYLON.Sound("galway", "res/music/galway.wav", scene, null, { loop: false, autoplay: false});
+  // var music8 = new BABYLON.Sound("hidden_past", "res/music/hidden_past.wav", scene, null, { loop: false, autoplay: false});
+  // var music9 = new BABYLON.Sound("one-eyed_maestro", "res/music/one-eyed_maestro.wav", scene, null, { loop: false, autoplay: false});
+  // var music10 = new BABYLON.Sound("parisian", "res/music/parisian.wav", scene, null, { loop: false, autoplay: false});
 
   music1.onEndedObservable.add(function(){
 	  music2.play();
     current_music = 2;
   });
-  music2.onEndedObservable.add(function(){
-	  music3.play();
-    current_music = 3;
-  });
-  music3.onEndedObservable.add(function(){
-	  music4.play();
-    current_music = 4;
-  });
-  music4.onEndedObservable.add(function(){
-	  music5.play();
-    current_music = 5;
-  });
-  music5.onEndedObservable.add(function(){
-	  music6.play();
-    current_music = 6;
-  });
-  music6.onEndedObservable.add(function(){
-	  music7.play();
-    current_music = 7;
-  });
-  music7.onEndedObservable.add(function(){
-	  music8.play();
-    current_music = 8;
-  });
-  music8.onEndedObservable.add(function(){
-	  music9.play();
-    current_music = 9;
-  });
-  music9.onEndedObservable.add(function(){
-	  music10.play();
-    current_music = 10;
-  });
-  music10.onEndedObservable.add(function(){
-	  music1.play();
-    current_music = 1;
-  });//end music
+  // music2.onEndedObservable.add(function(){
+	 //  music3.play();
+  //   current_music = 3;
+  // });
+  // music3.onEndedObservable.add(function(){
+	 //  music4.play();
+  //   current_music = 4;
+  // });
+  // music4.onEndedObservable.add(function(){
+	 //  music5.play();
+  //   current_music = 5;
+  // });
+  // music5.onEndedObservable.add(function(){
+	 //  music6.play();
+  //   current_music = 6;
+  // });
+  // music6.onEndedObservable.add(function(){
+	 //  music7.play();
+  //   current_music = 7;
+  // });
+  // music7.onEndedObservable.add(function(){
+	 //  music8.play();
+  //   current_music = 8;
+  // });
+  // music8.onEndedObservable.add(function(){
+	 //  music9.play();
+  //   current_music = 9;
+  // });
+  // music9.onEndedObservable.add(function(){
+	 //  music10.play();
+  //   current_music = 10;
+  // });
+  // music10.onEndedObservable.add(function(){
+	 //  music1.play();
+  //   current_music = 1;
+  // });//end music
 
-  // switch (current_music) {
-  //   case 1:
-  //     music1.setVolume(message.volume);
-  //     break;
-  //
-  //   case 2:
-  //     music2.setVolume(message.volume);
-  //     break;
-  //
-  //   case 3:
-  //     music3.setVolume(message.volume);
-  //     break;
-  //
-  //   case 4:
-  //     music4.setVolume(message.volume);
-  //     break;
-  //
-  //   case 5:
-  //     music5.setVolume(message.volume);
-  //     break;
-  //
-  //   case 6:
-  //     music6.setVolume(message.volume);
-  //     break;
-  //
-  //   case 7:
-  //     music7.setVolume(message.volume);
-  //     break;
-  //
-  //   case 8:
-  //     music8.setVolume(message.volume);
-  //     break;
-  //
-  //   case 9:
-  //     music9.setVolume(message.volume);
-  //     break;
-  //
-  //   case 10:
-  //     music10.setVolume(message.volume);
-  //     break;
-  // }
-
-  // if (message.music_pause == true) {
-  //   switch (current_music) {
-  //     case 1:
-  //       music1.pause();
-  //       break;
-  //
-  //     case 2:
-  //       music2.pause();
-  //       break;
-  //
-  //     case 3:
-  //       music3.pause();
-  //       break;
-  //
-  //     case 4:
-  //       music4.pause();
-  //       break;
-  //
-  //     case 5:
-  //       music5.pause();
-  //       break;
-  //
-  //     case 6:
-  //       music6.pause();
-  //       break;
-  //
-  //     case 7:
-  //       music7.pause();
-  //       break;
-  //
-  //     case 8:
-  //       music8.pause();
-  //       break;
-  //
-  //     case 9:
-  //       music9.pause();
-  //       break;
-  //
-  //     case 10:
-  //       music10.pause();
-  //       break;
-  //   }
-  // }
+  
 
   var volume_input = new BABYLON.GUI.Slider();
   volume_input.top = "-110px";
@@ -166,47 +82,32 @@ function createMusic(engine, canvas, message, database) {                       
   volume_input.value = 1;
   advTexture.addControl(volume_input);
   volume_input.onValueChangedObservable.add(function(value) {
-    switch (current_music) {
-      case 1:
+
         music1.setVolume(value);
-        break;
 
-      case 2:
-        music2.setVolume(value);
-        break;
 
-      case 3:
-        music3.setVolume(value);
-        break;
 
-      case 4:
-        music4.setVolume(value);
-        break;
+        music2.setVolume(value); 
 
-      case 5:
-        music5.setVolume(value);
-        break;
+   
+    //     music3.setVolume(value);
 
-      case 6:
-        music6.setVolume(value);
-        break;
+    //     music4.setVolume(value);
 
-      case 7:
-        music7.setVolume(value);
-        break;
+    //     music5.setVolume(value);
+ 
 
-      case 8:
-        music8.setVolume(value);
-        break;
+    //     music6.setVolume(value);
+    //     music7.setVolume(value);
 
-      case 9:
-        music9.setVolume(value);
-        break;
+    //     music8.setVolume(value);
 
-      case 10:
-        music10.setVolume(value);
-        break;
-    }
+    //     music9.setVolume(value);
+
+
+
+    //     music10.setVolume(value);
+     
   });
 
   var lute = BABYLON.GUI.Button.CreateImageWithCenterTextButton("lute_butt", "", "res/lute.png");
@@ -240,69 +141,69 @@ function createMusic(engine, canvas, message, database) {                       
         }
         break;
 
-      case 3:
-        if (music3.isPlaying) {
-          music3.pause();
-        } else if (music3.isPaused) {
-          music3.play();
-        }
-        break;
+      // case 3:
+      //   if (music3.isPlaying) {
+      //     music3.pause();
+      //   } else if (music3.isPaused) {
+      //     music3.play();
+      //   }
+      //   break;
 
-      case 4:
-        if (music4.isPlaying) {
-          music4.pause();
-        } else if (music4.isPaused) {
-          music4.play();
-        }
-        break;
+      // case 4:
+      //   if (music4.isPlaying) {
+      //     music4.pause();
+      //   } else if (music4.isPaused) {
+      //     music4.play();
+      //   }
+      //   break;
 
-      case 5:
-        if (music5.isPlaying) {
-          music5.pause();
-        } else if (music5.isPaused) {
-          music5.play();
-        }
-        break;
+      // case 5:
+      //   if (music5.isPlaying) {
+      //     music5.pause();
+      //   } else if (music5.isPaused) {
+      //     music5.play();
+      //   }
+      //   break;
 
-      case 6:
-        if (music6.isPlaying) {
-          music6.pause();
-        } else if (music6.isPaused) {
-          music6.play();
-        }
-        break;
+      // case 6:
+      //   if (music6.isPlaying) {
+      //     music6.pause();
+      //   } else if (music6.isPaused) {
+      //     music6.play();
+      //   }
+      //   break;
 
-      case 7:
-        if (music7.isPlaying) {
-          music7.pause();
-        } else if (music7.isPaused) {
-          music7.play();
-        }
-        break;
+      // case 7:
+      //   if (music7.isPlaying) {
+      //     music7.pause();
+      //   } else if (music7.isPaused) {
+      //     music7.play();
+      //   }
+      //   break;
 
-      case 8:
-        if (music8.isPlaying) {
-          music8.pause();
-        } else if (music8.isPaused) {
-          music8.play();
-        }
-        break;
+      // case 8:
+      //   if (music8.isPlaying) {
+      //     music8.pause();
+      //   } else if (music8.isPaused) {
+      //     music8.play();
+      //   }
+      //   break;
 
-      case 9:
-        if (music9.isPlaying) {
-          music9.pause();
-        } else if (music9.isPaused) {
-          music9.play();
-        }
-        break;
+      // case 9:
+      //   if (music9.isPlaying) {
+      //     music9.pause();
+      //   } else if (music9.isPaused) {
+      //     music9.play();
+      //   }
+      //   break;
 
-      case 10:
-        if (music10.isPlaying) {
-          music10.pause();
-        } else if (music10.isPaused) {
-          music10.play();
-        }
-        break;
+      // case 10:
+      //   if (music10.isPlaying) {
+      //     music10.pause();
+      //   } else if (music10.isPaused) {
+      //     music10.play();
+      //   }
+      //   break;
     }
   });
 
