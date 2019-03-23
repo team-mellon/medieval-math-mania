88ab56ffc920fe59b410b9e6f0ddb180b78b2c05
Eric Bitikofer <e.bitikofer@gmail.com>
2019-03-23 15:06:57 -0400

    backend setup

---
+++ server/routes/api/posts.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
+  // res.send("Hello");
+});
+
+// Add Post
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send();
+});
+
+// Delete Post
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
+});
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb+srv://belloq:1234@medieval-math-mania-dodmo.mongodb.net/test?retryWrites=true',
+    {
+      useNewUrlParser: true
+    }
+  );
+
+  return client.db('vue_express').collection('posts');
+}
+
+module.exports = router;
