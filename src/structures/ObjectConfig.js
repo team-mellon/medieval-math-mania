f88cc79878e4cf0799cc49085e887e677dcbfed7
Eric Bitikofer <e.bitikofer@gmail.com>
2020-04-04 12:46:03 -0400

    object config structure

---
+++ src/structures/ObjectConfig.js
@@ -0,0 +1,25 @@
+//==============================================================================
+//                                                                            ||
+// OBJECT CONFIG                                                              ||
+//============================================================================||
+//                                                                            ||
+// Initial configuration for generating and object                            ||
+//                                                                            ||
+//==============================================================================
+
+class ObjectConfig {
+
+  objectType;
+  type;
+
+  width;
+  height;
+
+  xLock;
+  xOffset;
+  yLock;
+  yOffset;
+
+}
+
+export default ObjectConfig;
