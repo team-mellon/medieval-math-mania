4f87853c737c561d4177b657b47108f88ff8d803
Eric Bitikofer <e.bitikofer@gmail.com>
2020-04-20 02:04:22 -0400

    refactor(scaling): comments and consisitency

--- src/components/Scaling.vue
+++ src/components/Scaling.vue
@@ -29,6 +29,30 @@
           maxStored: false,
         },
 
+        // Ratio breakpoint
+        ratioBreakpoint: 2.5,
+
+        // X scaling
+        x: 1.0,
+
+        // Y scaling
+        y: 1.0,
+
+        // The max y value
+        maxY: 1440,
+
+        // The max x value
+        maxX: 1920,
+
+        // Initial screen ratio
+        ratio: 1440 / 1920,
+
+        // X margin to center rendering
+        xMargin: 0.0,
+
+        // If the max is stored
+        maxStored: false,
+
       }
     },
     props: {
@@ -60,40 +84,112 @@
        */
       calculateScaling: function() {
 
-        this.scale.ratio = this.$props.width / this.$props.height;
+        // Calc the canavas aspect ratio
+        this.calculateScreenRatio();
 
-        // Calculate the scene scaling
-        if (this.scale.ratio < 2.5) { // tall screen
+        // If this is a tall screen...
+        if (this.scale.ratio < this.ratioBreakpoint) {
 
-          // Calculate the scale with the max x scaling
-          this.scale.maxStored = false;
-          this.scale.x = this.$props.width / this.scale.maxX;
-          this.scale.y = this.$props.width / this.scale.maxX;
-          let key = 'sceneScaleY';
-          sessionStorage.setItem(key, this.scale.y);
+          // Calculate the scale using the max x resolution size
+          this.scaleForTallScreen();
 
-        } else if (this.scale.ratio > 2.5) { // wide screen
+        // If this is a wide screen...
+        } else if (this.scale.ratio > this.ratioBreakpoint) {
 
-          let tempScale = 1;
-          let tempMax = 1440;
-
-          if(!this.scale.maxStored) {
-            this.scale.maxStored = true;
-            tempMax = this.$props.height;
-          }
-
-          tempScale = this.$props.width / this.scale.maxX;
-          this.scale.x = tempScale * ( this.$props.height / tempMax );
-          this.scale.y = tempScale * ( this.$props.height / tempMax );
-          let key = 'sceneScaleY';
-          sessionStorage.setItem(key, this.scale.y);
+          // Calculate the scale using the max x resolution size
+          this.scaleForWideScreen();
 
         }
+
+        // Get the scene margin
+        this.getSceneMargin();
+
+      },
+
+      /**
+       * Calculate the scene scaling.
+       */
+      calculateScreenRatio: function() {
+
+        // Set the ratio to the canvases aspect ratio.
+        this.scale.ratio = this.$props.width / this.$props.height;
+
+      },
+
+      /**
+       * Calculate scaling for tall screens.
+       */
+      scaleForTallScreen: function() {
+
+        // Set the flag to false.
+        this.scale.maxStored = false;
+
+        // Set the x scale to the canvas width divided by the max x size.
+        this.scale.x = this.$props.width / this.scale.maxX;
+
+        // Set the y scale to the canvas width divided by the max x size.
+        this.scale.y = this.$props.width / this.scale.maxX;
+
+        // Update storage
+        this.setStorage();
+
+      },
+
+      /**
+       * Calculate scaling for wide screens.
+       */
+      scaleForWideScreen: function() {
+
+        // Set an initial scale.
+        let tempScale = 1;
+
+        // Set a temporary max scale
+        let tempMax = 1440;
+
+        // If the max x res is not stored
+        if(!this.scale.maxStored) {
+
+          // Set it to true.
+          this.scale.maxStored = true;
+
+          // Set the temp max to the canvas height
+          tempMax = this.$props.height;
+
+        }
+
+        // Set the temp scale to the canvas width divided by the max x scale.
+        tempScale = this.$props.width / this.scale.maxX;
+
+        // Set the x scale to the canvas height divided by the temp max multiplied by the temp scale.
+        this.scale.x = tempScale * ( this.$props.height / tempMax );
+
+        // Set the y scale to the canvas height divided by the temp max multiplied by the temp scale.
+        this.scale.y = tempScale * ( this.$props.height / tempMax );
+
+        // Update storage
+        this.setStorage();
+
+      },
+
+      /**
+       * Calculate the scenes x margin to center the rendering on the screen.
+       */
+      getSceneMargin: function() {
 
         // Calculate the scene margin in a given direction
         this.scale.xMargin = ( this.$props.width - this.scale.maxX ) / 2;
 
-      }
+      },
+
+      /**
+       * Update vars in session storage.
+       */
+      setStorage: function() {
+
+        // Set the y scale for the scen in the session storage.
+        sessionStorage.setItem('sceneScaleY', this.scale.y);
+
+      },
 
     }
 
